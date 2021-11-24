08d374e60b6b4025ddfdae7e6f2de722cca8e150
caH40 <kmvsancho@gmail.com>
2021-11-24 20:09:56 +0300

    repear lognewday

--- app_modules/log.js
+++ app_modules/log.js
@@ -3,11 +3,11 @@ const logerror = require('./logerror')
 
 const getDate = () => new Date().toLocaleDateString()
 const dirDate = () => ((new Date().getMonth()) + 1) + '.' + (new Date().getFullYear())
-const pathDir = `./logs/log/${dirDate()}`
-const path = `./logs/log/${dirDate()}/${getDate()}.json`
-const creatNewDir = () => { fs.mkdirSync(pathDir) }
+const pathDir = () => `./logs/log/${dirDate()}`
+const path = () => `./logs/log/${dirDate()}/${getDate()}.json`
+const creatNewDir = () => { fs.mkdirSync(pathDir()) }
 const creatNewLog = () => {
-	fs.writeFile(path, '[]', err => {
+	fs.writeFile(path(), '[]', err => {
 		if (err) {
 			logerror(err)
 		}
@@ -18,21 +18,21 @@ const creatNewLog = () => {
 // update информация о запросе
 const addLog = async function (update) {
 	// проверка наличия папки mm.yyyy, если нет то создаем
-	if (!fs.existsSync(pathDir)) {
+	if (!fs.existsSync(pathDir())) {
 		creatNewDir()
 	}
 	//проверка наличия файла с текущей датой
-	if (!fs.existsSync(path)) {
+	if (!fs.existsSync(path())) {
 		await creatNewLog()
 	}
-	fs.readFile(path, 'utf-8', (err, content) => {
+	fs.readFile(path(), 'utf-8', (err, content) => {
 		if (err) {
 			logerror(err)
 		}
 		try {
 			const contentArr = JSON.parse(content)
 			contentArr.push(update)
-			fs.writeFile(path, JSON.stringify(contentArr), err => {
+			fs.writeFile(path(), JSON.stringify(contentArr), err => {
 				if (err) {
 					logerror(err)
 				}
