9f05246171bbfe6068a1df9337d5af0a8cd3677e
caH40 <kmvsancho@gmail.com>
2022-06-30 13:46:03 +0300

    убрал предпросмотр ссылок при ссылке в телеге

--- app_modules/bike-master.js
+++ app_modules/bike-master.js
@@ -1,7 +1,9 @@
 const text = require('./texts')
 
 async function bikeMaster(bot) {
-	await bot.reply(text.bikeMaster, { parse_mode: 'html' }).catch(error => console.log(error))
+	await bot
+		.reply(text.bikeMaster, { parse_mode: 'html', disable_web_page_preview: true })
+		.catch(error => console.log(error))
 }
 
 module.exports = bikeMaster
