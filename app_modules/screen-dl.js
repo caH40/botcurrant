8ae0b3d2e8f5a414fed7fecc80fd1475936a948e
caH40 <kmvsancho@gmail.com>
2022-08-23 08:36:07 +0300

    fixed a problem with paths in the screenshots module

--- app_modules/screen-dl.js
+++ app_modules/screen-dl.js
@@ -1,20 +1,20 @@
+const path = require('path');
 const download = require('image-downloader');
 
 async function screenDownLoad(webcam, bot) {
 	try {
 		let options = {
 			url: `https://gw.cmo.sai.msu.ru/${webcam}.jpg`,
-			dest: `./images/${webcam}.jpg`
+			dest: path.resolve('./', 'images', `${webcam}.jpg`),
 		};
-		await download.image(options)
-			.catch(() => {
-				bot.reply('Что то пошло не так, есть более ранняя фотография...')
-			})
+		await download.image(options).catch(e => {
+			console.log(e);
+			bot.reply('Что то пошло не так, есть более ранняя фотография...');
+		});
 		await bot.replyWithPhoto({ source: `./images/${webcam}.jpg` });
 	} catch (error) {
 		console.log(error);
 	}
-
 }
 
-module.exports = screenDownLoad
+module.exports = screenDownLoad;
