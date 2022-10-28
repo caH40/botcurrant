72e0a070453ba75f8c8ca79b6803258780165420
caH40 <kmvsancho@gmail.com>
2022-10-28 12:53:21 +0300

    added a new bikeMaster

--- app_modules/texts.js
+++ app_modules/texts.js
@@ -5,6 +5,7 @@ const text = {
 		`Вячеслав, г.Кисловодск,\nтел: <b>+7 928 970-80-80</b>,\n<a href="https://www.google.com/maps/place/ВЕЛО+МОТО+26,+ул.+Андрея+Губина,+62,+Кисловодск,+Ставропольский+край,+357736/data=!4m2!3m1!1s0x40582bac3b2b2b0f:0x121e7bfa6b811562?utm_source=mstt_1&entry=gps&g_ep=CAESBzExLjM1LjEYACD___________8BKgA%3D">www.google.com/maps</a>\n\n`,
 		`Веломастерская "Фристайл", г.Пятигорск,\nтел: <b>+7 961 477-00-99</b>,\n<a href="https://vk.com/free_styleshop">"Фристайл"</a>\n\n`,
 		`"Freeride" (mtb), г.Ессентуки,\nтел: <b>+7 963 384-45-94</b>,\n<a href="https://www.google.com/maps/place/Freeride,+Привокзальный+рынок+павильон+45,+Железнодорожная+ул.,+1,+Ессентуки,+Ставропольский+край,+357601/data=!4m2!3m1!1s0x40578496c899e399:0x9d74ff479e807e82?utm_source=mstt_1&entry=gps&g_ep=CAESBzExLjM1LjEYACD___________8BKgA%3D">www.google.com/maps</a>\n\n`,
+		`"Veloki", г.Кисловодск,\nтел: <b>+7 928 326-27-84</b>,\n<a href="https://veloki.ru/">veloki.ru</a>\n\n`,
 		`"Чемпион", г.Ессентуки,\nтел: <b>+7 963 381-13-88</b>,\n<a href="https://yandex.ru/maps/org/chempion/200132832286/?ll=42.841743%2C44.041688&z=15.51">www.google.com/maps</a>\n\n`,
 	],
 };
