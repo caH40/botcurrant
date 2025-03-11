7c2b29fa6603c70403fee6cfa06f65bc6520925e
Alexander Berezhnev <freelancerbav@gmail.com>
2025-03-11 11:56:28 +0300

    Обновлено меню полезная информация. Удалена погода.

--- keyboards/information.js
+++ keyboards/information.js
@@ -1,44 +1,44 @@
 export function keyboardInfo(isPrivate) {
-	return [
-		[
-			{
-				text: 'Анонсы и результаты соревнований', // текст на кнопке
-				url: 'https://t.me/results_cycling_KMV',
-			},
-		],
-		isPrivate
-			? [
-					{
-						text: 'Результаты соревнований ДжилыСу', // текст на кнопке
-						web_app: { url: 'https://bike-caucasus.ru/dzhilsu' },
-					},
-			  ]
-			: [],
-		isPrivate
-			? [
-					{
-						text: 'Велосипедные маршруты', // текст на кнопке
-						web_app: { url: 'https://bike-caucasus.ru/trails' },
-					},
-			  ]
-			: [],
-		[
-			{
-				text: 'Веломастерские на КМВ', // текст на кнопке
-				callback_data: 'bikeMaster',
-			},
-		],
-		[
-			{
-				text: 'Погода на завтра', // текст на кнопке
-				callback_data: 'weatherTomorrow',
-			},
-		],
-		[
-			{
-				text: 'Погода на выходные', // текст на кнопке
-				callback_data: 'weatherWeekend',
-			},
-		],
-	];
+  return [
+    [
+      {
+        text: 'Анонсы и результаты соревнований', // текст на кнопке
+        url: 'https://t.me/results_cycling_KMV',
+      },
+    ],
+    // isPrivate
+    //   ? [
+    //       {
+    //         text: 'Результаты соревнований ДжилыСу', // текст на кнопке
+    //         web_app: { url: 'https://bike-caucasus.ru/dzhilsu' },
+    //       },
+    //     ]
+    //   : [],
+    isPrivate
+      ? [
+          {
+            text: 'Велосипедные маршруты', // текст на кнопке
+            web_app: { url: 'https://bike-caucasus.ru/trails' },
+          },
+        ]
+      : [],
+    [
+      {
+        text: 'Веломастерские на КМВ', // текст на кнопке
+        callback_data: 'bikeMaster',
+      },
+    ],
+    // [
+    // 	{
+    // 		text: 'Погода на завтра', // текст на кнопке
+    // 		callback_data: 'weatherTomorrow',
+    // 	},
+    // ],
+    // [
+    // 	{
+    // 		text: 'Погода на выходные', // текст на кнопке
+    // 		callback_data: 'weatherWeekend',
+    // 	},
+    // ],
+  ];
 }
